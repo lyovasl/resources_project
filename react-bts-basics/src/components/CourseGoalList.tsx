acb2d555418dadd7b571e4d463c100a8e85955a8
Levon Aslanyan <aslanyan2012@mail.ru>
2024-02-21 13:44:23 +0400

    summary section finished

--- react-bts-basics/src/components/CourseGoalList.tsx
+++ react-bts-basics/src/components/CourseGoalList.tsx
@@ -1,4 +1,7 @@
+import { ReactNode } from "react";
+
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox.tsx";
 import { type CourseGoal as CGoals } from "../App.tsx";
 
 type GoalsListProps = {
@@ -10,15 +13,36 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: GoalsListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goal yet. Start adding some.
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You`re collecting a lot of goals. Dont put too much on year plate
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
